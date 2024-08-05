1236245cf6e39eadc391103832bac0ed1ecdd686
sureshbabu170302 <sureshbabu.kanini@gmail.com>
2024-08-05 13:36:31 +0530

    Initial Commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,42 @@
+const fetchButton = document.getElementById("fetchBtn");
+const listElements = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+
+function sendHttpRequest(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.send(JSON.stringify(data));
+  });
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  const listOfPosts = JSON.parse(responseData);
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElements.append(postEl);
+  }
+}
+
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
+fetchButton.addEventListener("click", fetchPosts);
+createPost("DUMMY", "A dummy post");
